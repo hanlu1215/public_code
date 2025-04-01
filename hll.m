5095bce0de68a4d26f6b29c75968b12125cf6e49
韩路路 <hanlu0306@hotmail.com>
2025-04-01 17:28:17 +0800

    在 hll.m 中添加字体设置的注释，以便于后续调整

--- hll.m
+++ hll.m
@@ -1,6 +1,10 @@
 clear;close all;clc
 set(0, 'DefaultAxesFontName', 'Times New Roman'); % 设置图形坐标轴字体
 set(0, 'DefaultTextFontName', 'Times New Roman'); % 设置文本字体
+% set(groot, 'DefaultAxesFontName', 'FangSong');
+% set(groot, 'DefaultTextFontName', 'FangSong');
+% set(groot, 'DefaultAxesFontName', 'Arial');
+% set(groot, 'DefaultTextFontName', 'Arial');
 % set(0, 'DefaultAxesFontSize', 14);
 % set(0, 'DefaultTextFontSize', 14);
 
